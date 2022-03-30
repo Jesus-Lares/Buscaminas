bb9c85f33711084f91e2d6fb77805350b7daa964
Jesus Lares Contreras <jesus.lares2000@gmail.com>
2022-03-30 01:04:00 -0600

    Se agrego el header (falta que funcione el boton de opciones)

--- files/main.py
+++ files/main.py
@@ -1,27 +1,58 @@
 import pygame
 import sys
 
-width=400
-height=400
+from utils import *
+from ui import *
 
-def main():
-    pygame.init()
-    pygame.display.set_mode((width,height))
-    pygame.display.set_caption("Buscaminas")
+class Main():
+    def __init__(self):
+        pygame.init()
+        # Declarar fuentes
+        self.bigFont = pygame.font.SysFont("arial",25)
+        self.smallFont = pygame.font.SysFont("arial",18)
 
-    clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((WIDTH,HEIGHT))
+        pygame.display.set_caption("Buscaminas")
 
-    openInterface = True
+        self.screen.fill(BACKGROUND)
+        self.cursor = Cursor()  
+        clock = pygame.time.Clock()
 
-    while openInterface:
-        clock.tick(60)
+        self.btnHeaderSettings, self.textNumMines = self.headerDesign() 
+        self.numMines=15
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        openInterface = True
+        while openInterface:
+            clock.tick(60)
+            
+            self.cursor.update() 
+            self.btnHeaderSettings.update(self.screen,self.cursor)
+            self.textNumMines.update(str(self.numMines).rjust(2, '0')) ##Posibilidad de mover el 2 al redondear el numero de bombas que se tiene
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    if self.cursor.colliderect(self.btnHeaderSettings.getRect()):
+                        print("click")
+                        self.numMines-=1
+            
+            pygame.display.flip()
+        
 
-        pygame.display.flip()
-    
+    def headerDesign(self):
+        pygame.draw.rect(self.screen,HEADER,(0,0,WIDTH,SIZE_HEADER))
+
+        #Numero de bombas
+        imgMines = pygame.image.load("images/mina.png")
+        pictureMines = pygame.transform.scale(imgMines,[40,40])
+        self.screen.blit(pictureMines,[0,0])
+        
+        textNumMines = Text(self.screen,str(self.numMines),40,5,self.bigFont) 
+        btnSettings = Button(self.screen,"Opciones",WIDTH-110,7,self.smallFont)
+        
+        return btnSettings,textNumMines
+
 
 if __name__ == "__main__":
-    main()
+    Main()
--- files/utils.py
+++ files/utils.py
@@ -1,20 +1,29 @@
 
-# COLORS in RGB
-GRAY          = (127  , 127   , 127   , 255)
-SILVER        = (192  , 192   , 192   , 255)
-RED           = (255  , 0     , 0     , 255)
-GREEN         = (0    , 255   , 0     , 255)
-BLUE          = (0    , 0     , 255   , 255)
-YELLOW        = (255  , 255   , 0     , 255)
-CYAN          = (0    , 255   , 255   , 255)
-MAGENTA       = (255  , 0     , 255   , 255)
-BLACK         = (0    , 0     , 0     , 255)
-WHITE         = (255  , 255   , 255   , 255)
-DARK_RED      = (100  , 0     , 0     , 255)
-DARK_GREEN    = (0    , 100   , 0     , 255)
-DARK_BLUE     = (0    , 0     , 100   , 255)
-DARK_YELLOW   = (100  , 100   , 0     , 255)
-DARK_CYAN     = (0    , 100   , 100   , 255)
-DARK_MAGENTA  = (100  , 0     , 100   , 255)
-PURPLE        = (85   , 26    , 139   , 255)
-GOLDEN        = (255  , 215   , 0     , 255)
+# Measures
+WIDTH=400
+HEIGHT=400
+BORDER=4
+SIZE_HEADER=40
+
+# Colors in RGB
+GRAY          = (127  , 127   , 127)
+SILVER        = (192  , 192   , 192)
+RED           = (255  , 0     , 0  )
+GREEN         = (0    , 255   , 0  )
+BLUE          = (0    , 0     , 255)
+YELLOW        = (255  , 255   , 0  )
+CYAN          = (0    , 255   , 255)
+MAGENTA       = (255  , 0     , 255)
+BLACK         = (0    , 0     , 0  )
+WHITE         = (255  , 255   , 255)
+DARK_RED      = (100  , 0     , 0  )
+DARK_GREEN    = (0    , 100   , 0  )
+DARK_BLUE     = (0    , 0     , 100)
+DARK_YELLOW   = (100  , 100   , 0  )
+DARK_CYAN     = (0    , 100   , 100)
+DARK_MAGENTA  = (100  , 0     , 100)
+PURPLE        = (85   , 26    , 139)
+GOLDEN        = (255  , 215   , 0  )
+BACKGROUND    = (219  , 226   , 239)
+HEADER        = (17   , 45    , 78 )
+PRIMARY       = (63   , 114   , 175)
